9d1aec2852bb55fc8f3da3c35347b4570c9a0e05
yebbbbiiii <k2932673@naver.com>
2024-03-27 10:14:09 +0900

    vsbin.py

--- vsbin.py
+++ vsbin.py
@@ -1,1 +1,12 @@
-#include <stdio.h>
+from gtts import gTTS
+import playsound
+
+text = ""
+
+file_name = ".mp3"
+
+tts = gTTS(text=text, lang="ko")
+
+tts.save(file_name)
+
+playsound.playsound(file_name)
