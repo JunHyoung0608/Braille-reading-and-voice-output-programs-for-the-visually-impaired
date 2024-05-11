5036676bc41a6a1c7d03a5b802966e5b1fae724e
yebbbbiiii <113655039+yebbbbiiii@users.noreply.github.com>
2024-05-11 21:19:46 +0900

    Update decoder_56.py

--- decoder_56.py
+++ decoder_56.py
@@ -1,6 +1,6 @@
 #5/10 12:16am
 #고민해볼것 : 모음 2번(똑같이 if문으로 제한)/ 12: ㅆ 고민 / index 고민(pop말고 del로 해결 완료)
-#현재 문제 : 약어 BE, 숫자 
+#현재 문제 : 약자 BE, 숫자, 약어
 
 INITIAL_LIST = {8: 'ㄱ', 9: 'ㄴ', 10: 'ㄷ', 16: 'ㄹ', 17: 'ㅁ', 24: 'ㅂ', 32: 'ㅅ', 40: 'ㅈ', 48: 'ㅊ', 11: 'ㅋ', 19: 'ㅌ', 25: 'ㅍ', 26: 'ㅎ'}
 NEUTRALITY_LIST = {35: 'ㅏ', 28: 'ㅑ', 14: 'ㅓ', 49: 'ㅕ', 37: 'ㅗ', 44: 'ㅛ', 13: 'ㅜ', 41: 'ㅠ', 42: 'ㅡ', 21: 'ㅣ', 23: 'ㅐ', 29: 'ㅔ', 12: 'ㅖ', 39: 'ㅘ', 61: 'ㅚ', 15: 'ㅝ', 58: 'ㅢ'}
@@ -15,7 +15,7 @@ SAME = [32]
 DIR = [36] # 모음 + 모음이 왔을때 따로 쓰이는 붙임법/가운데 36 들어오면 따로 쓰인다. 
 # ㅑ,ㅐ -> ㅒ, ㅝ,ㅐ ->ㅞ, ㅘ,ㅐ -> ㅙ, ㅜ,ㅐ -> ㅟ / 붙어있으면 ㅒ,ㅞ,ㅙ,ㅟ로 읽히게 한다. 
 
-input_data = [0,1,14,0,35,18,9,49,54,26,32,29,44,40,14,9,53,8,21,34,12,24,21,18,21,3,9,21,43,3,4,39,23]
+input_data = [0,1,14,0,35,18,9,49,54,26,32,29,44,40,14,9,53,8,21,34,12,24,21,18,21,3,9,21,10,43,3,4,39,23,60,1,1,1,0]
 
 result = []
 prev_type = None
@@ -34,7 +34,28 @@ for i, code in enumerate(input_data):
     else:
         prev_code = None
 
-    if code in INITIAL_LIST: # 초성 
+    #if code in NUM: #숫자
+    #    while input_data[i+1] != 0:
+    #        i += 1 
+    #        key = input_data[i]
+    #        result.append(NUM_LIST[key])
+    #        del input_data[i]
+
+
+    if code == 60: #숫자
+        end_index = i + 1
+        while end_index < len(input_data) and input_data[end_index] != 0:
+            end_index += 1
+        result.extend([NUM_LIST[key] for key in input_data[i + 1:end_index]])
+        del input_data[i + 1:end_index]
+        i += 1  # 현재 숫자 뒤에 있는 0도 삭제해야 하므로 인덱스 증가
+
+    elif (input_data[i] == 1) and (input_data[i-1] == 0) and (input_data[i+1] in AND_LIST) and (input_data[i+2] == 0):
+        i += 1; key = input_data[i]
+        del input_data[i]
+        result.append(AND_LIST[key])
+    
+    elif code in INITIAL_LIST or code in SHORT_LIST_BE: # 초성 
         if next_code in NEUTRALITY_LIST or next_code in SHORT_LIST_AF: # 중성과 약어 앞에 올 경우
             result.append(INITIAL_LIST[code])
         elif code == 32 and next_code in [8,10,24,32,40]: #현재 숫자가 32이면서 뒤에 5개의 숫자가 오면 ㄲ,ㄸ,ㅃ,ㅆ,ㅉ로 출력해라 
@@ -54,6 +75,10 @@ for i, code in enumerate(input_data):
             elif next_code == 40:
                 del input_data[i]
                 result.append('ㅉ')
+        elif next_code not in NEUTRALITY_LIST or next_code in FINAL_LIST:
+            result.append(SHORT_LIST_BE[code])
+        
+
 
     elif code in NEUTRALITY_LIST: #중성 
         if prev_code not in INITIAL_LIST: # 앞에 초성이 오지 않을때 
@@ -81,6 +106,8 @@ for i, code in enumerate(input_data):
             else:
                 result.append('ㅇ')
                 result.append(NEUTRALITY_LIST[code])
+        elif prev_code in INITIAL_LIST:
+            result.append(NEUTRALITY_LIST[code])
    
         elif prev_code in INITIAL_LIST: #앞에 초성 올때
             if code in [28,39,15,13] and next_code == 23:
@@ -158,11 +185,12 @@ for i, code in enumerate(input_data):
         elif prev_code in SHORT_LIST_BE or next_code not in FINAL_LIST:
             result.append(FINAL_LIST[code])   
 
-    elif code in SHORT_LIST_BE: #약자 가,나,다 등 
-        if next_code in FINAL_LIST or prev_code in FINAL_LIST: #뒤에 종성이 올 경우
-            result.append(SHORT_LIST_BE[code])
-        else: # 뒤에 종성이 오지 않는다면 
-            result.append(SHORT_LIST_BE[code])
+    #elif code in SHORT_LIST_BE: #약자 가,나,다 등 
+    #    if next_code in INITIAL_LIST or next_code in FINAL_LIST or next_code in SHORT_LIST_BE or next_code == 0: #만약 뒤에 초성이 오면 
+    #        result.append(SHORT_LIST_BE[code])
+    #    else:)
+    #        result.append('x')
+
 
     elif code in SHORT_LIST_AF: #약자 억,언,얼 등 
         if prev_code in INITIAL_LIST and next_code not in FINAL_LIST: #앞에 초성은 올 수 있지만 뒤에 종성은 올수 없음
@@ -173,14 +201,5 @@ for i, code in enumerate(input_data):
     elif code in JUMP: #띄어쓰기
         result.append(JUMP[code])
 
-    elif code in NUM: # 숫자 
-        while input_data[i] != 0:
-            result.append(NUM_LIST[next_code])
-            i += 1
-#약어
-    elif (code == 1) and (input_data[prev_code] == 0) and (input_data[next_code] in AND_LIST) and (input_data[i+2] == 0):
-        i += 1
-        result.append(AND_LIST[next_code])
-
-
 print(result)
+
